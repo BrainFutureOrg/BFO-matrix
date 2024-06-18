7ef29501100ab58114fee2acceeb08526b2092bc
ShkarupyloMaksym <shkarupilo2005@gmail.com>
2024-06-18 16:25:18 +0300

    Add random letters files

---
+++ src/random_letter.c
@@ -0,0 +1,5 @@
+//
+// Created by maksym on 18.06.24.
+//
+
+#include "random_letter.h"
---
+++ src/random_letter.h
@@ -0,0 +1,8 @@
+//
+// Created by maksym on 18.06.24.
+//
+
+#ifndef BFO_MATRIX_RANDOM_LETTER_H
+#define BFO_MATRIX_RANDOM_LETTER_H
+
+#endif //BFO_MATRIX_RANDOM_LETTER_H
