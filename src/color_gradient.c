d9897ab1bb894fab5083cd1d648239bc8090c436
KosenkoAlexander <alexandkosenko@gmail.com>
2024-06-18 16:36:45 +0300

    color_gradient[.c,.h] added

---
+++ src/color_gradient.c
@@ -0,0 +1,5 @@
+//
+// Created by kosenko on 18.06.24.
+//
+
+#include "color_gradient.h"
---
+++ src/color_gradient.h
@@ -0,0 +1,8 @@
+//
+// Created by kosenko on 18.06.24.
+//
+
+#ifndef BFO_MATRIX_SRC_COLOR_GRADIENT_H
+#define BFO_MATRIX_SRC_COLOR_GRADIENT_H
+
+#endif //BFO_MATRIX_SRC_COLOR_GRADIENT_H
