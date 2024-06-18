ab35113cfb0528fe69b7ca898ce853e303090743
KosenkoAlexander <alexandkosenko@gmail.com>
2024-06-18 19:35:01 +0300

    color gradient interpolation generalized

--- src/color_gradient.c
+++ src/color_gradient.c
@@ -5,8 +5,7 @@
 #include "color_gradient.h"
 #include <stdlib.h>
 
-int* color_gradient_get(color_gradient_settings settings, double position){
-    int* rgb = malloc(3* sizeof(int));
+int* color_gradient_get_general(color_gradient_settings settings, double position, color_interpolator interpolator){
     int position_after=0;
     for(int i=0; i<settings.len; i++){
         if(position<=settings.positions[i]){
@@ -15,15 +14,26 @@ int* color_gradient_get(color_gradient_settings settings, double position){
         }
     }
     if(position_after==0){
+        int* rgb = malloc(3* sizeof(int));
         rgb[0] = settings.colors[0][0];
         rgb[1] = settings.colors[0][1];
         rgb[2] = settings.colors[0][2];
+        return rgb;
     }else{
         double interval = settings.positions[position_after]-settings.positions[position_after-1];
         double before_norm = (position-settings.positions[position_after-1])/interval;
-        rgb[0] = (int)(settings.colors[position_after-1][0]*(1-before_norm)+settings.colors[position_after][0]*before_norm);
-        rgb[1] = (int)(settings.colors[position_after-1][1]*(1-before_norm)+settings.colors[position_after][1]*before_norm);
-        rgb[2] = (int)(settings.colors[position_after-1][2]*(1-before_norm)+settings.colors[position_after][2]*before_norm);
+        return interpolator(settings.colors[position_after-1], settings.colors[position_after], before_norm);
     }
+}
+
+int* linear_color_interpolator(int* rgb1, int* rgb2, double position){
+    int* rgb = malloc(3* sizeof(int));
+    rgb[0] = (int)(rgb1[0]*(1-position)+rgb2[0]*position);
+    rgb[1] = (int)(rgb1[1]*(1-position)+rgb2[1]*position);
+    rgb[2] = (int)(rgb1[2]*(1-position)+rgb2[2]*position);
     return rgb;
+}
+
+int* color_gradient_get(color_gradient_settings settings, double position){
+    return color_gradient_get_general(settings, position, linear_color_interpolator);
 }
--- src/color_gradient.h
+++ src/color_gradient.h
@@ -11,6 +11,20 @@ typedef struct{
     int len;
 } color_gradient_settings;
 
+typedef int* (*color_interpolator)(int* rgb1, int* rgb2, double position);
+
+// calculate color at certain position in gradient
+// position must be normalized to fit in [0,1]
+// color is linearly interpolated
 int* color_gradient_get(color_gradient_settings settings, double position);
+
+// calculate color at certain position in gradient
+// position must be normalized to fit in [0,1]
+// interpolator is applied between every pair of colors
+int* color_gradient_get_general(color_gradient_settings settings, double position, color_interpolator interpolator);
+
+// interpolates r, g and b linearly between two points
+// position should be normalized to fit in [0,1]
+int* linear_color_interpolator(int* rgb1, int* rgb2, double position);
 
 #endif //BFO_MATRIX_SRC_COLOR_GRADIENT_H
