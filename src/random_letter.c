8a531482da9c370d74fca30a2a3182b38506c355
ShkarupyloMaksym <shkarupilo2005@gmail.com>
2024-06-19 11:17:14 +0300

    Add function for generating random character

--- src/random_letter.c
+++ src/random_letter.c
@@ -2,4 +2,13 @@
 // Created by maksym on 18.06.24.
 //
 
+#include <locale.h>
 #include "random_letter.h"
+#include "library/random_bfo/statistical_random.h"
+
+wchar_t get_random_unicode_char(){
+    int codepoint = randint(0, 0xFFFF + 1); // Generate a random number between 0 and 0xFFFF
+
+    wchar_t character = codepoint;
+    return character;
+}
