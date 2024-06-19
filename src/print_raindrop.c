c3d5e3ada0f1e2b2150703263ed9f8233fa0c80d
KosenkoAlexander <alexandkosenko@gmail.com>
2024-06-19 12:43:22 +0300

    print_raindrop() and fill_bg() implemented

--- src/print_raindrop.c
+++ src/print_raindrop.c
@@ -3,3 +3,32 @@
 //
 
 #include "print_raindrop.h"
+#include "library/terminal_bfo/terminal_funcs.h"
+
+void print_raindrop(rain_drop raindrop, int row_num, int col_num, print_raindrop_settings settings){
+    printf("%s", settings.background.line);
+    if(raindrop.column<col_num){
+        int end = raindrop.size+raindrop.row;
+        end = end > col_num?col_num:end;
+        for(int i=raindrop.row; i<end;i++){
+            terminal_goto_xy(raindrop.column+1, i+1);
+            int position = i-raindrop.row;
+            double position_norm = (double)(position+raindrop.size-raindrop.used)/(raindrop.size-1);
+            unsigned char* rgb = color_gradient_get_general(settings.gradient_settings, position_norm, settings.interpolator);
+            COLOR color = color_create_foreground_rgb(rgb[0], rgb[1], rgb[2]);
+            printf("%s%lc", color.line, raindrop.line[position]);
+            free_color(color);
+            free(rgb);
+        }
+    }
+}
+
+void fill_bg(int row_num, int col_num, print_raindrop_settings settings){
+    printf("%s", settings.background.line);
+    for(int i=0; i++<row_num;){
+        for(int j=0; j++<col_num;){
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
--- src/print_raindrop.h
+++ src/print_raindrop.h
@@ -4,5 +4,18 @@
 
 #ifndef BFO_MATRIX_SRC_PRINT_RAINDROP_H
 #define BFO_MATRIX_SRC_PRINT_RAINDROP_H
+#include "rain.h"
+#include "color_gradient.h"
+#include "library/terminal_bfo/colors_bfo/colors.h"
+
+typedef struct{
+    color_gradient_settings gradient_settings;
+    color_interpolator interpolator;
+    COLOR background;
+}print_raindrop_settings;
+
+void print_raindrop(rain_drop raindrop, int row_num, int col_num, print_raindrop_settings settings);
+
+void fill_bg(int row_num, int col_num, print_raindrop_settings settings);
 
 #endif //BFO_MATRIX_SRC_PRINT_RAINDROP_H
